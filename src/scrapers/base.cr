078254eb8c755cd3ed19080e531d372112dd793d
Serhat Seyren <sseyren@hotmail.com>
2021-06-13 20:58:37 +0300

    set_timeout method in BaseScraper

--- src/scrapers/base.cr
+++ src/scrapers/base.cr
@@ -8,6 +8,18 @@ module RssScrapers
       def initialize(@path : String, @title : String, @target : URI)
       end
 
+      # Should call this method whenever a `HTTP::Client` constructed.
+      protected def set_timeout(
+        client : HTTP::Client,
+        timeout = DEFAULT_TIMEOUT,
+      )
+        client.connect_timeout = timeout
+        client.dns_timeout = timeout
+        client.read_timeout = timeout
+        client.write_timeout = timeout
+        client
+      end
+
       def run
         raise NotImplementedError.new "run"
       end
