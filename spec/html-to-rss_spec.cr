d77924250ae70ed3cbacf3439c4a540d1a2498e7
Serhat Seyren <sseyren@hotmail.com>
2021-06-14 15:53:23 +0300

    HtmlToRss: tests added

--- spec/html-to-rss_spec.cr
+++ spec/html-to-rss_spec.cr
@@ -1,9 +1,46 @@
-require "./spec_helper"
+require "spec-kemal"
+require "../src/html-to-rss"
+
+include HtmlToRss
 
 describe HtmlToRss do
-  # TODO: Write tests
+  it "renders page for known paths" do
+    get "/"
+    response.status_code.should eq 200
 
-  it "works" do
-    false.should eq(true)
+    get "/#{SCRAPER_TREE[0].path}"
+    response.status_code.should eq 200
+  end
+
+  it "returns 404 for unknown paths" do
+    get "/a-nonexistent-organization-path"
+    response.status_code.should eq 404
+
+    get "/a-nonexistent-organization-path/wtf"
+    response.status_code.should eq 404
+
+    get "/rss/a-nonexistent-organization-path"
+    response.status_code.should eq 404
+
+    get "/rss/a-nonexistent-organization-path/general"
+    response.status_code.should eq 404
+
+    get "/rss/a-nonexistent-organization-path/general/wtf"
+    response.status_code.should eq 404
+  end
+
+  it "returns 404 for known organizations but unknown paths" do
+    get "/rss/#{SCRAPER_TREE[0].path}/this-is-not-a-valid-path-for-this-scraper"
+    response.status_code.should eq 404
+  end
+
+  # i think that's enough
+  it "fetches for first scrapers of all organizations that registered" do
+    SCRAPER_TREE.each do |org|
+      get "/rss/#{org.path}/#{org.scrapers[0].path}"
+
+      response.status_code.should eq 200
+      response.content_type.should eq "text/xml"
+    end
   end
 end
